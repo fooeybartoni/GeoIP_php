5aa8749eb95a7664f3836981ba40fdb0fcc7b666
bruce <bruce@derecho.io>
2014-04-30 10:35:02 -0400

    looks good final version

--- maxmind/test.php
+++ maxmind/test.php
@@ -2,31 +2,80 @@
 require_once '../vendor/autoload.php';
 use GeoIp2\Database\Reader;
 
-// This creates the Reader object, which should be reused across
-// lookups.
-$reader = new Reader("/Users/brucegoldfeder/Sites/GeoIP/GeoLite2-City.mmdb");
+// Initiating function
+$IP_Geo_Array = createIPGeoArray();
 
-// File to read in, this can be refactored to input arg for generalization
-$IPfile = "/Users/brucegoldfeder/Sites/GeoIP/hits_http.txt";
+print_r($IP_Geo_Array);
 
-getIPfromCSV($reader, $IPfile);
+function createIPGeoArray(){
 
-function getIPfromCSV($reader, $IPfile) {
-	
+	// This creates the Reader object, which should be reused across
+	// lookups.
+	$reader = new Reader("/Users/brucegoldfeder/Sites/GeoIP/GeoLite2-City.mmdb");
+
+	// File to read in, this can be refactored to input arg for generalization
+	$IPfile = "/Users/brucegoldfeder/Sites/GeoIP/hits_http.txt";
+
+	// loops through the ip to geo function to create an ID defined list of IP Geo Data
+	// Creates an array of arrays with the ID of the counter and an IP-Geo data set
+
+	$cnt = 1;
+
+	$InnerArray = array();
+
+	$IP_Geo_Assoc = array();
+
+	$IParray = getIPfromCSV($IPfile);
+
+	foreach ( $IParray as $ip_inner ) {
+		
+		$IP_Geo_Assoc[$cnt] = getIPGeo($ip_inner, $reader);
+
+		$cnt++;
+	}
+
+	return $IP_Geo_Assoc;
+
+}
+
+function getIPfromCSV($IPfile) {
+
 	//read in the IP listings
 	$file_handle = fopen($IPfile, "rw");
+
+	$result = array();
 
 	while (!feof($file_handle) ) {
 
 		$line_of_text = fgetcsv($file_handle, 1024);
 
-		$IPtoGeo = $line_of_text[1];
-
-		$calcIP = printIPGeo($IPtoGeo, $reader);
+		$result[] = $line_of_text[1];
 
 	}
 
 	fclose($file_handle);
+
+	return $result;
+}
+
+function getIPGeo($IPtoGeo, $reader) {
+
+	$record = $reader->city($IPtoGeo);
+
+	$Geo_IPlist = array(
+			'IP' => $IPtoGeo,
+			'Country_Code' => $record->country->isoCode,
+			'Country_Name' => $record->country->name,
+			'State_Code' => $record->mostSpecificSubdivision->isoCode,
+			'State_Name' => $record->mostSpecificSubdivision->name,
+			'City_Name' => $record->city->name,
+			'Postal_Code' => $record->postal->code,
+			'Lat' => $record->location->latitude,
+			'Lon' => $record->location->longitude
+		);
+
+	return $Geo_IPlist;	
+
 }
 
 // Replace "city" with the appropriate method for your database, e.g.,
@@ -34,21 +83,23 @@ function getIPfromCSV($reader, $IPfile) {
 function printIPGeo($IPtoGeo, $reader) {
 	$record = $reader->city($IPtoGeo);
 
-	print('IP to Geo is: ' . $IPtoGeo . "\n");
+	
 
-	print($record->country->isoCode . "\n"); // 'US'
-	print($record->country->name . "\n"); // 'United States'
+	print('IP to Geo is: ' . $IPtoGeo . ", ");
 
+	print($record->country->isoCode . ", "); // 'US'
+	print($record->country->name . ", "); // 'United States'
 
-	print($record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
-	print($record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
+	print($record->mostSpecificSubdivision->isoCode . ", "); // 'MN'
+	print($record->mostSpecificSubdivision->name . ", "); // 'Minnesota'
 
-	print($record->city->name . "\n"); // 'Minneapolis'
+	print($record->city->name . ", "); // 'Minneapolis'
 
-	print($record->postal->code . "\n"); // '55455'
+	print($record->postal->code . ", "); // '55455'
 
-	print($record->location->latitude . "\n"); // 44.9733
-	print($record->location->longitude . "\n"); // -93.2323
+	print($record->location->latitude . ", "); // 44.9733
+	print($record->location->longitude . ", "); // -93.2323
 	print ("<BR>");
 }
+
 ?>
